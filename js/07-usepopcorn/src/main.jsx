8572179e5e07a2119d46cf962815c330c650d41c
madhu-1999 <madhulikasawant@gmail.com>
2025-08-28 14:50:00 -0700

    StarRating component

--- js/07-usepopcorn/src/main.jsx
+++ js/07-usepopcorn/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+//import './index.css'
+//import App from './App.jsx'
+import { StarRating } from "./StarRating";
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    {/*<App />*/}
+    <StarRating
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+      defaultRating={3}
+    />
+  </StrictMode>
+);
